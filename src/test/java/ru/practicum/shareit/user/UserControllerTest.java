1f6b78c15b47f82e33154c3e6218b6157dd97502
Gyuzyal <gyuzyal.sergeychuk@yandex.ru>
2023-08-15 20:37:13 +0300

    fix: исправление замечаний по пр

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
