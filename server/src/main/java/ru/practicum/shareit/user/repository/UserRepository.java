824f7969dc0bc8dc652098707a01600e8d718bd1
Гюзяль <gyuzyal.sergeychuk@yandex.ru>
2023-08-27 23:48:41 +0300

    fix user

--- server/src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ server/src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findById(Long id);
+
     Optional<User> findByEmail(String email);
 
     @Query("SELECT u From User u " +
